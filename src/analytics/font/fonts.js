628c9ae1b5b6380166325b0f89c0dd32c602070c
ms.moradi <ms.moradi28@gmail.com>
2023-08-03 21:23:21 +0330

    add fonts for google fonts

---
+++ src/analytics/font/fonts.js
@@ -0,0 +1,17 @@
+import { Nunito, Heebo, Pacifico } from 'next/font/google';
+
+export const nunito = Nunito ({
+    subsets:['latin'],
+    display:'swap',
+})
+
+export const heebo = Heebo ({
+    subsets:['latin'],
+    display:'swap',
+})
+
+export const pacifico = Pacifico ({
+    subsets:['latin'],
+    display:'swap',
+    weight:'400',
+})

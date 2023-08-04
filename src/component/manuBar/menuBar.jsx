c162be65d41258ee733251515258cdaf47aca0d0
ms.moradi <ms.moradi28@gmail.com>
2023-08-04 16:55:21 +0330

    add display

--- src/component/manuBar/menuBar.jsx
+++ src/component/manuBar/menuBar.jsx
@@ -1,20 +1,19 @@
-'use client'
 import Link from 'next/link';
 
-
-const MenuBar =()=>{
+const MenuBar =({display})=>{
     return(
-        <>
-        <ul>
-            <Link href="/"><li>HOME</li></Link>
-            <Link href="/About"><li>ABOUT</li></Link>
-            <Link href="/Services"><li>SERVICES</li></Link>
-            <Link href="/Menu"><li>MENU</li></Link>
-            <li>PAGES</li>
-            <Link href="/Contact"><li>CONTACT</li></Link>
-        </ul>
-        </>
-    )
+        <div className={`${display}`}>
+            <ul className='block'> 
+                <Link href="/"><li>HOME</li></Link>
+                <Link href="/About"><li>ABOUT</li></Link>
+                <Link href="/Services"><li>SERVICES</li></Link>
+                <Link href="/Menu"><li>MENU</li></Link>
+                <li>PAGES</li>
+                <Link href="/Contact"><li>CONTACT</li></Link>
+            </ul>
+        </div>
+    )    
+    
 }
 
 export default MenuBar;

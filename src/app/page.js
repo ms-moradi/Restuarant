861b7eebf8efc46be6909b5a88c43af8ecd71a19
ms.moradi <ms.moradi28@gmail.com>
2023-08-04 16:53:36 +0330

    replace navBar to MenuBar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import { pacifico } from '@/analytics/font/fonts'
 import Image from 'next/image'
-import MenuBar from '@/component/manuBar/menuBar' 
-
+import NavBar from '@/component/navBar/navBar'
+import MenuBar from '@/component/manuBar/menuBar'
 
 export default function Home() {
   return (
     <main >
-      <MenuBar/>
+      <NavBar/>
       <p className={`${pacifico.className}`}>Home</p>
     </main>
   )

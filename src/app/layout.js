8aff98530eea0d90bed1da5ad456737252b4c35b
ms.moradi <ms.moradi28@gmail.com>
2023-08-05 15:03:47 +0330

    import font awesome

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import {heebo} from '../analytics/font/fonts';
-
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const metadata = {
   title: 'Restuarant',
@@ -10,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={heebo.className}>
+      <head/>
       <body>{children}</body>
     </html>
   )

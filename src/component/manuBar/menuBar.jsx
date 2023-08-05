055632a2db071d9ac37520dfed0f830a676a4219
ms.moradi <ms.moradi28@gmail.com>
2023-08-05 15:03:06 +0330

    change style

--- src/component/manuBar/menuBar.jsx
+++ src/component/manuBar/menuBar.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const MenuBar =({display})=>{
     return(
-        <div className={`${display}`}>
-            <ul className='block'> 
+        <div className='bg-[#0f172b] px-4'>
+            <ul className= {`${display} text-[#ffffff] gap-3 border-t-[.1px] border-[#93969f] lg:border-t-0 px-3 py-4`}> 
                 <Link href="/"><li>HOME</li></Link>
                 <Link href="/About"><li>ABOUT</li></Link>
                 <Link href="/Services"><li>SERVICES</li></Link>

5b264435507e3baa7359b3d8c33c3d6918a61a2a
ms.moradi <ms.moradi28@gmail.com>
2023-08-06 12:21:45 +0330

    import navbar and submenu

---
+++ src/component/header/header.jsx
@@ -0,0 +1,25 @@
+'use client'
+import { useState } from 'react';
+
+import NavBar from '../navBar/navBar';
+import Submenu from '@/component/submenu/submenu';
+
+const Header =()=>{
+
+    const [screenUser,setScreenUser] = useState(screen.availWidth)
+    const [openSubMenu, setOpenSubMenu] = useState(false)
+
+
+    return(
+        <>
+        <NavBar openSubMenu={openSubMenu} setOpenSubMenu={setOpenSubMenu} screenUser={screenUser} setScreenUser={setScreenUser}/>
+        {openSubMenu?
+            <Submenu display={'inline-block'}/>
+        :
+            <Submenu display={'hidden'}/>
+        }
+        </>
+    )
+}
+
+export default Header;

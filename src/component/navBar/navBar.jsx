3538327439ce406845e63ce4e723fb219b05afd6
ms.moradi <ms.moradi28@gmail.com>
2023-08-06 12:20:36 +0330

    add icon and style

--- src/component/navBar/navBar.jsx
+++ src/component/navBar/navBar.jsx
@@ -2,59 +2,50 @@
 
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faUtensils
-} from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 //import components
 import MenuBar from "../manuBar/menuBar";
+
 
 //import font 
 import { nunito } from "@/analytics/font/fonts";
 
 
 
-const NavBar =()=>{
+const NavBar =({setOpenSubMenu, openSubMenu, screenUser, setScreenUser})=>{
 
     const [openMenu, setOPenMenu] = useState(false)
     const [displayMenu, setDisplayMenu] = useState('hidden')
-    const [screenUser, setScreenUser] = useState(screen.availWidth)
-
-    //get the user's screen size
-
-    // if(widthUserScreen<1424){
-    //     setScreenUser(true)
-    // }else{
-    //     setScreenUser(false)
-    // }
 
     //handel open menu and display menubar
     const handelOpenMenu=()=>{
         setOPenMenu(current => !current)
         setDisplayMenu(openMenu? 'block': 'hidden')
+        setOpenSubMenu(!openMenu && false)
     }
-    console.log(screenUser)
+    
 
     return(
-        <div>
-            <div className="bg-[#0f172b] flex flex-row justify-between gap-2 px-6 py-4">
+        <div className="fixed top-0 right-0 left-0">
+            <div className="bg-[#0f172b] flex flex-row justify-between lg:items-center gap-2 px-[15px] lg:pl-[50px] lg:pr-[30px">
                 <div className="flex justify-start items-center gap-2">
-                    <FontAwesomeIcon icon={faUtensils} className="text-[#fea116] sm:text-2xl lg:text-4xl"/>   
+                    <FontAwesomeIcon icon={faUtensils} className="text-[#fea116] text-2xl lg:text-4xl"/>   
                     <h1 className={`${nunito.className} text-[#fea116] text-[26.8px] lg:text-[38px] font-extrabold not-italic `} >
                         Restuarant
                     </h1>
                 </div>
                 {screenUser< 1024 ?
-                    <button onClick={handelOpenMenu} className="text-[#93969f]  border-black">
-                     Menu
+                    <button onClick={handelOpenMenu} className=" text-[#93969f]">
+                     <FontAwesomeIcon icon={faBars} className="text-[20px]" />
                     </button>
                 :
-                    <MenuBar display={'flex'}/>
+                    <MenuBar display={'flex'} setOpenSubMenu={setOpenSubMenu}/>
                 }
             </div>
-            <div className="">
-                <MenuBar display={displayMenu}/>
+            <div>
+                <MenuBar display={displayMenu} setOpenSubMenu={setOpenSubMenu} screenUser={screenUser} openSubMenu={openSubMenu}/>
             </div>
         </div>
     )

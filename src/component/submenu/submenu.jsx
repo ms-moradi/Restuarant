aadc6bdf6d4fce199f1b3296e58cbe2015c93a16
ms.moradi <ms.moradi28@gmail.com>
2023-08-06 12:20:08 +0330

    create and style

---
+++ src/component/submenu/submenu.jsx
@@ -0,0 +1,15 @@
+
+import Link from "next/link";
+
+const Submenu =({display})=>{
+    return(
+            <ul className={`lg:w-[120px] lg:h-[150px] ${display} bg-white text-[#000] sm lg:absolute lg:top-[70px] lg:right-[270px] lg:z-10`}>
+                <Link href="/Booking"><li className="hover:bg-blue-100 cursor-pointer">Booking</li></Link>
+                <Link href="/Team"><li className="hover:bg-blue-100 cursor-pointer">Our Team</li></Link>
+                <Link href="/Testimonial"><li className="hover:bg-blue-100 cursor-pointer">Testimonial</li></Link>
+            </ul>
+        
+    )
+}
+
+export default Submenu;

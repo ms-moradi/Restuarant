d97cee79899e079905ca047f7434b6c42e070719
ms.moradi <ms.moradi28@gmail.com>
2023-08-09 19:20:48 +0330

    add Service components

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Banner from '@/component/banner/banner';
-
+import Service from '@/component/service/service';
 
 
 export default function Home() {
 
   return (
     <>
-      {/* <div className=''>hello</div> */}
       <Banner/>
+      <Service/>
     </>
   )
 }

b0f57ee1e3f3d32fd2d45470769c793b3be6c699
ms.moradi <ms.moradi28@gmail.com>
2023-08-09 19:17:15 +0330

    change style

--- src/component/submenu/submenu.jsx
+++ src/component/submenu/submenu.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Submenu =({display})=>{
     return(
-            <ul className={`lg:w-[120px] lg:h-[150px] ${display} bg-white text-[#000] sm lg:absolute lg:top-[70px] lg:right-[270px] lg:z-10`}>
+            <ul className={`lg:w-[120px] lg:h-[80px] ${display} bg-white text-[#000] sm lg:absolute lg:top-[70px] lg:right-[270px] lg:z-10`}>
                 <Link href="/Booking"><li className="hover:bg-blue-100 cursor-pointer">Booking</li></Link>
                 <Link href="/Team"><li className="hover:bg-blue-100 cursor-pointer">Our Team</li></Link>
                 <Link href="/Testimonial"><li className="hover:bg-blue-100 cursor-pointer">Testimonial</li></Link>

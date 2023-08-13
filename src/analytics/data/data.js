8737a9f8bd44cfe576ce361d56567f8857f5179f
ms.moradi <ms.moradi28@gmail.com>
2023-08-13 18:45:43 +0330

    add data

---
+++ src/analytics/data/data.js
@@ -0,0 +1,20 @@
+import menu1 from "@/analytics/image/menu-1.jpg";
+import menu2 from "@/analytics/image/menu-2.jpg";
+import menu3 from "@/analytics/image/menu-3.jpg";
+import menu4 from "@/analytics/image/menu-4.jpg";
+import menu5 from "@/analytics/image/menu-5.jpg";
+import menu6 from "@/analytics/image/menu-6.jpg";
+import menu7 from "@/analytics/image/menu-7.jpg";
+import menu8 from "@/analytics/image/menu-8.jpg";
+
+
+export const foodMenu = [
+    {id:1, picture:menu1, status:0},
+    {id:2, picture:menu2, status:1},
+    {id:3, picture:menu3, status:2},
+    {id:4, picture:menu4, status:0},
+    {id:5, picture:menu5, status:1},
+    {id:6, picture:menu6, status:2},
+    {id:7, picture:menu7, status:3},
+    {id:8, picture:menu8, status:4},
+]

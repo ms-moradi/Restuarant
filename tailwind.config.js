ae016ed929c4af185065b8254b2be12fc77b96ff
ms.moradi <ms.moradi28@gmail.com>
2023-08-13 17:23:23 +0330

    add boxShadow and safelist for animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    safelist:['animate-[fade-in-up_1s_ease-in-out]'],
     extend: {
-      backgroundImage: {
-        
+      boxShadow:{
+        '3xl': '5px 5px 40px 5px rgba(0, 0, 0, 0.1)'
       },
       animation:{
         'spin-slow': 'spin 30s linear infinite',
+        'fade-in-up': 'fade-in-up 1s ease-in-out',
       },
     },
   },

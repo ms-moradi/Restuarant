18d7fdab5f208951dd2caf1bbfef85effb674baf
ms.moradi <ms.moradi28@gmail.com>
2023-08-13 17:26:53 +0330

    add Welcome and FoodMenu components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Banner from '@/component/banner/banner';
 import Service from '@/component/service/service';
+import Welcome from '@/component/welcome/welcome';
+import FoodMenu from '@/component/foodMenu/foodMenu';
 
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
     <>
       <Banner/>
       <Service/>
+      <Welcome/>
+      <FoodMenu/>
     </>
   )
 }

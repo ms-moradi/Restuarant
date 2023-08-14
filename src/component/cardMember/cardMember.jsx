911e158f5f9bdb751a4b726e7f243c42fbf5878e
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 18:39:10 +0330

    add use state and display

--- src/component/cardMember/cardMember.jsx
+++ src/component/cardMember/cardMember.jsx
@@ -1,30 +1,41 @@
 'use client'
 
+import { useState } from "react";
 import Image from "next/image";
 import { nunito, heebo } from "@/analytics/font/fonts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {sociaNletwork } from "@/analytics/data/data";
-import team1 from "@/analytics/image/team-1.jpg";
+
 
 const CardMember =({picture, name ,description})=>{
+    const[display, setDisplay] = useState(false);
+
+    const handelOnMouseEnter=()=>{
+        setDisplay(true)
+    }
+
+    const handleMOuseLeave =()=>{
+        setDisplay(false)
+    }
+
     return(
-        <div className="w-[260px] inline-block shadow-3xl p-6">
-            <div className=" flex flex-col items-center gap-6 pb-6">
-                <div className="w-[200px] rounded-full overflow-hidden">
-                    <Image src={picture} alt="team" className="hover:scale-125 hover:ease-in-out duration-300"/>
+        <div className="w-[260px] inline-block shadow-3xl" onMouseEnter={handelOnMouseEnter} onMouseLeave={handleMOuseLeave}>
+            <div className=" flex flex-col items-center gap-6 p-6">
+                <div className="w-[200px] rounded-full overflow-hidden ">
+                    <Image src={picture} alt="team" className='hover:scale-115'/>
                 </div>
                 <div className="flex flex-col items-center">
                     <h5 className={`${nunito.className} text-xl font-extrabold`}>{name}</h5>
                     <p className={`${heebo.className} text-[#666565] text-sm font-normal`}>{description}</p>
                 </div>
             </div>
-            {/* <div>
+            <div className={display ? 'flex justify-center gap-2' : 'hidden'}>
                 {sociaNletwork.map(network =>(
-                    <div key={network.id} className="w-[30px] flex justify-center bg-[#fea116] rounded-t-full p-3">
+                    <div key={network.id} className="w-[30px] flex justify-center bg-[#fea116] rounded-t-full p-2">
                         <FontAwesomeIcon icon={network.icon} className="text-[#fff] text-lg"/>
                     </div>
                 ))}
-            </div> */}
+            </div>
         </div>
     )
 }

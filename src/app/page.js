02395c40dc74089ea5329ee5c9975290bc4b8306
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 15:34:07 +0330

    add teamMember Component

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Service from '@/component/service/service';
 import Welcome from '@/component/welcome/welcome';
 import FoodMenu from '@/component/foodMenu/foodMenu';
 import BookTable from '@/component/bookTable/bookTable';
+import TeamMember from '@/component/teamMember/teamMember';
 
 export default function Home() {
 
@@ -12,7 +13,8 @@ export default function Home() {
       <Service/>
       <Welcome/>
       <FoodMenu/>
-      <BookTable/>
+      {/* <BookTable/> */}
+      <TeamMember/>
     </>
   )
 }

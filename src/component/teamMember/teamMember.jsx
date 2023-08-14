d7b74144192106daa6fefece71ff9f06569420bd
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 15:40:21 +0330

    add display to div countainer

--- src/component/teamMember/teamMember.jsx
+++ src/component/teamMember/teamMember.jsx
@@ -3,7 +3,7 @@ import CardMember from "../cardMember/cardMember";
 
 const TeamMember =()=>{
     return(
-        <div>
+        <div className="flex justify-center gap-6">
             {members.map(member=>(
                 <div key={member.id}>
                     <CardMember picture={member.image} name={member.name} description={member.description}/>

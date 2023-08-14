64a9f3a9bf7a80fd2ffe640daff5bb4746d289f3
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 18:38:45 +0330

    add element and style

--- src/component/teamMember/teamMember.jsx
+++ src/component/teamMember/teamMember.jsx
@@ -1,14 +1,24 @@
 import { members } from "@/analytics/data/data";
 import CardMember from "../cardMember/cardMember";
+import{ pacifico, nunito } from "@/analytics/font/fonts";
 
 const TeamMember =()=>{
     return(
-        <div className="flex justify-center gap-6">
+        <div className="flex flex-col items-center gap-2 mb-4">
+            <div className="flex justify-center items-center gap-2">
+                <div className="w-[40px] h-[2px] bg-[#fea116]"></div>
+                <h5 className={`${pacifico.className} text-xl text-[#fea116] font-normal`}>Team Member</h5>
+                <div className="w-[40px] h-[2px] bg-[#fea116]"></div>
+            </div>
+            <h1 className={`${nunito.className} text-[40px] font-extrabold`}>Our Master Chefs</h1>
+            <div className="flex justify-center gap-6 my-10">
             {members.map(member=>(
                 <div key={member.id}>
                     <CardMember picture={member.image} name={member.name} description={member.description}/>
                 </div>
             ))}
+            </div>
+            
         </div>
     )
 }

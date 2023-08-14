0caed478a96646a1c1a8c7a9293206bc4c3d278a
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 15:37:37 +0330

    add props

--- src/component/cardMember/cardMember.jsx
+++ src/component/cardMember/cardMember.jsx
@@ -6,16 +6,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {sociaNletwork } from "@/analytics/data/data";
 import team1 from "@/analytics/image/team-1.jpg";
 
-const CardMember =()=>{
+const CardMember =({picture, name ,description})=>{
     return(
         <div className="w-[220px] inline-block shadow-3xl p-6">
             <div className=" flex flex-col items-center gap-6 pb-6">
                 <div className="rounded-full overflow-hidden">
-                    <Image src={team1} alt="team" className="hover:scale-125"/>
+                    <Image src={picture} alt="team" className="hover:scale-125"/>
                 </div>
                 <div className="flex flex-col items-center">
-                    <h5 className={`${nunito.className} text-xl font-extrabold`}>Full Name</h5>
-                    <p className={`${heebo.className} text-[#666565] text-sm font-normal`}>Designation</p>
+                    <h5 className={`${nunito.className} text-xl font-extrabold`}>{name}</h5>
+                    <p className={`${heebo.className} text-[#666565] text-sm font-normal`}>{description}</p>
                 </div>
             </div>
             <div>

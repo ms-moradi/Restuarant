351e6dd38b64a93c2724959b03eaa543f3d4ac2e
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 18:42:14 +0330

    add props

--- src/component/cardComment/cardComment.jsx
+++ src/component/cardComment/cardComment.jsx
@@ -5,18 +5,18 @@ import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import testimonial3 from "@/analytics/image/testimonial-3.jpg";
 
 
-const CardComment =()=>{
+const CardComment =({name, image, commnet, profession })=>{
     return(
         <div className="w-[360px] border border-slate-200 rounded-sm p-6">
             <FontAwesomeIcon icon={faQuoteLeft} className="text-[#fea116] text-5xl" />
             <div className=" py-3">
-                <p className="text-[#666565] text-base font-normal">Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</p>  
+                <p className="text-[#666565] text-base font-normal">{comment}</p>  
             </div>
             <div className="flex justidy-start items-center gap-4">
-                <Image src={testimonial3} alt="user image" width={50} className="rounded-full"/>
+                <Image src={image} alt="user image" width={50} className="rounded-full"/>
                 <div className="">
-                    <h5 className={`${nunito.className} text-xl font-extrabold`}>Client Neme</h5>
-                    <p className={`${heebo.className} text-[#666565] text-sm font-normal`}>Profession</p>
+                    <h5 className={`${nunito.className} text-xl font-extrabold`}>{name}</h5>
+                    <p className={`${heebo.className} text-[#666565] text-sm font-normal`}>{profession}</p>
                 </div>
             </div>
         </div>

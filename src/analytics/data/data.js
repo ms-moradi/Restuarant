6475e47d6831b60cedc55282114ab4f905232abc
ms.moradi <ms.moradi28@gmail.com>
2023-08-14 15:31:32 +0330

    add data

--- src/analytics/data/data.js
+++ src/analytics/data/data.js
@@ -1,3 +1,5 @@
+import { faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 import menu1 from "@/analytics/image/menu-1.jpg";
 import menu2 from "@/analytics/image/menu-2.jpg";
 import menu3 from "@/analytics/image/menu-3.jpg";
@@ -6,6 +8,10 @@ import menu5 from "@/analytics/image/menu-5.jpg";
 import menu6 from "@/analytics/image/menu-6.jpg";
 import menu7 from "@/analytics/image/menu-7.jpg";
 import menu8 from "@/analytics/image/menu-8.jpg";
+import team1 from "@/analytics/image/team-1.jpg";
+import team2 from "@/analytics/image/team-2.jpg";
+import team3 from "@/analytics/image/team-3.jpg";
+import team4 from "@/analytics/image/team-4.jpg";
 
 
 export const foodMenu = [
@@ -17,4 +23,17 @@ export const foodMenu = [
     {id:6, picture:menu6, status:2},
     {id:7, picture:menu7, status:3},
     {id:8, picture:menu8, status:4},
+]
+
+export const sociaNletwork = [
+    {id:1, icon:faFacebookF},
+    {id:2, icon:faTwitter},
+    {id:3, icon: faInstagram},
+]
+
+export const members = [
+    {id:1, name:'FullName', description:'Designation', image:team1},
+    {id:2, name:'FullName', description:'Designation', image:team2},
+    {id:3, name:'FullName', description:'Designation', image:team3},
+    {id:4, name:'FullName', description:'Designation', image:team4},
 ]

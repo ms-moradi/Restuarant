cce8515bd9eb4137c47b5941d0bb552af28ad182
ms.moradi <ms.moradi28@gmail.com>
2023-08-16 13:39:22 +0330

    add div and import Titlefooter componnet and data

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,12 +1,18 @@
 'use client'
 
 import {pacifico, nunito} from '@/analytics/font/fonts';
+import TitleFooter from '../titleFooter/titleFooter';
 
 const Footer =()=>{
     return(
         <div>
-            <div>
-
+            <div className='flex justify-between'>
+                <div>
+                    <TitleFooter title={'Company'}/>
+                </div>
+                <div>
+                    <TitleFooter title={'Contact'}/>
+                </div>
             </div>
             <div></div>
         </div>

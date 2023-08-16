8b09cdf99a0b69b85e7687a2391ac1d556f8f708
ms.moradi <ms.moradi28@gmail.com>
2023-08-16 13:55:55 +0330

    add background color to container and style

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -9,21 +9,22 @@ import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Footer =()=>{
     return(
-        <div>
+        <div className='bg-[#0F172B]'>
             <div className='flex justify-between'>
                 <div>
                     <TitleFooter title={'Company'}/>
                     <ul>
                         {companyFooter.map(item => (
-                            <div key={item.id}>
-                                <FontAwesomeIcon icon={faAngleRight} />
-                                <li>{item.title}</li>
+                            <div key={item.id} className='flex justify-start items-center gap-2 mb-2'>
+                                <FontAwesomeIcon icon={faAngleRight} className='text-[#fff] text-[12px]'/>
+                                <li className={`${nunito.className} text-[15px] text-[#fff] font-normal`}>{item.title}</li>
                             </div>
                         ))}
                     </ul>
                 </div>
                 <div>
                     <TitleFooter title={'Contact'}/>
+                    
                 </div>
             </div>
             <div></div>

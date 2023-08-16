0435f87ef0b17d8f60db94ed945b6f5b1709a4c8
ms.moradi <ms.moradi28@gmail.com>
2023-08-16 13:56:32 +0330

    add margin bottom

--- src/component/titleFooter/titleFooter.jsx
+++ src/component/titleFooter/titleFooter.jsx
@@ -3,7 +3,7 @@ import {pacifico} from "@/analytics/font/fonts";
 
 const TitleFooter =({title})=>{
     return(
-        <div class="flex justify-center items-center gap-2">
+        <div class="flex justify-center items-center gap-2 mb-5">
             <h4 className={`${pacifico.className} text-2xl text-[#fea116] font-normal`}>{title}</h4>
             <div className="w-[40px] h-[2px] bg-[#fea116]"></div>
         </div>

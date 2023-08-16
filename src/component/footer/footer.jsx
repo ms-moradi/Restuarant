ff7bc46e0aa45f41c89f3763cba75a03326d8f6e
ms.moradi <ms.moradi28@gmail.com>
2023-08-16 13:15:44 +0330

    create file and import font

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import {pacifico, nunito} from '@/analytics/font/fonts';
+
+const Footer =()=>{
+    return(
+        <div>
+            <div>
+
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Footer;

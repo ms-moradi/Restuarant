868e6d664f09c09a72953fcb108adfd807e1921b
ms.moradi <ms.moradi28@gmail.com>
2023-08-16 13:46:51 +0330

    add fontAwesome icon and ul element

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -2,6 +2,10 @@
 
 import {pacifico, nunito} from '@/analytics/font/fonts';
 import TitleFooter from '../titleFooter/titleFooter';
+import { companyFooter } from '@/analytics/data/data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 
 const Footer =()=>{
     return(
@@ -9,6 +13,14 @@ const Footer =()=>{
             <div className='flex justify-between'>
                 <div>
                     <TitleFooter title={'Company'}/>
+                    <ul>
+                        {companyFooter.map(item => (
+                            <div key={item.id}>
+                                <FontAwesomeIcon icon={faAngleRight} />
+                                <li>{item.title}</li>
+                            </div>
+                        ))}
+                    </ul>
                 </div>
                 <div>
                     <TitleFooter title={'Contact'}/>

57368b953cc668693ba0933d663733496b081f77
ms.moradi <ms.moradi28@gmail.com>
2023-08-20 16:49:18 +0330

    add keyframe and animate ping-slow

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,12 @@ module.exports = {
       animation:{
         'spin-slow': 'spin 30s linear infinite',
         'fade-in-up': 'fade-in-up 1s ease-in-out',
+        'ping-slow': 'ping 2s cubic-bezier(0, 0, .2, 1) infinite'
+      },
+      keyframes:{
+        ping:{
+          '95% , 100%': {transform: 'scale(1)', opacity: '0'}
+        }
       },
     },
   },

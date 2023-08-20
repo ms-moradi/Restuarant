2ee567c9ee460e315e58cf132ecf934b95acbfb5
ms.moradi <ms.moradi28@gmail.com>
2023-08-20 16:57:36 +0330

    add welcome and TeamMember components

--- src/app/(pages)/About/page.jsx
+++ src/app/(pages)/About/page.jsx
@@ -1,7 +1,12 @@
+import Welcome from "@/component/welcome/welcome";
+import TeamMember from "@/component/teamMember/teamMember";
+
 
 const About = ()=>{
     return(
         <>
+        <Welcome/>
+        <TeamMember/>
         </>
     )
 }

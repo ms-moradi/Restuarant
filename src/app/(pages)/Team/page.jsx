23d6da1d7279722d158a086dcdc59d1a681b7f8c
ms.moradi <ms.moradi28@gmail.com>
2023-08-20 16:59:30 +0330

    add Title and Member components

--- src/app/(pages)/Team/page.jsx
+++ src/app/(pages)/Team/page.jsx
@@ -1,11 +1,13 @@
-
-
+import Title from "@/component/title/title";
+import Member from "@/component/member/member";
 
 const Team =()=>{
     return(
-        <>
-        Team
-        </>
+        <div className="flex flex-col items-center justify-center my-10" >
+            <Title title={'Team Members'} header={'Our Master Chefs'}/>
+            <Member/>
+            <Member/>
+        </div>
     )
 }
 

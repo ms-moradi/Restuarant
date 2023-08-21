529cf2f70180ad6be41d3975a5eb64fa938e0f30
ms.moradi <ms.moradi28@gmail.com>
2023-08-21 18:43:28 +0330

    add animation

--- src/component/service/service.jsx
+++ src/component/service/service.jsx
@@ -6,17 +6,17 @@ import { nunito, heebo } from "@/analytics/font/fonts";
 
 
 const data = [
-    {id:1, title:'Master Chefs', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faUserTie},
-    {id:2, title:'Quality Food', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faUtensils},
-    {id:3, title:'Online Order', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faCartPlus},
-    {id:4, title:'24/7 Service', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faHeadphonesSimple},
+    {id:1, title:'Master Chefs', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faUserTie, time:0.5},
+    {id:2, title:'Quality Food', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faUtensils, time:0.75},
+    {id:3, title:'Online Order', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faCartPlus, time:1},
+    {id:4, title:'24/7 Service', discription:'Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam', icon:faHeadphonesSimple, time:1.25},
 ]
 
 const Service =()=>{
     return(
         <div className='flex justify-center content-center gap-[25px] my-10 px-[60px]'>
             {data.map((card) => (
-                <div key={card.id} className="inline-block w-[260px] h-[260px] hover:bg-[#fea116] hover:text-[#fff] p-6 shadow-3xl rounded-[2px] animate-fade-in-up">
+                <div key={card.id} className={`inline-block w-[260px] h-[260px] hover:bg-[#fea116] hover:text-[#fff] p-6 shadow-3xl rounded-[2px] animate-[slide-in_1s_ease-in-out_1s]`}>
                     <FontAwesomeIcon icon={card.icon} className="text-[#fea116] text-[48px] mb-5" />
                     <h5 className={`${nunito.className} text-[20px] font-extrabold mb-3`}>{card.title}</h5>
                     <div className="w-[190px]">

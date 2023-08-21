a6d5afed6900fd7bea59b6d97935d0798b97dd0d
ms.moradi <ms.moradi28@gmail.com>
2023-08-21 18:42:53 +0330

    add height css

--- src/component/banner/banner.jsx
+++ src/component/banner/banner.jsx
@@ -7,12 +7,12 @@ import hero from '@/analytics/image/hero.png';
 
 const Banner =()=>{
     return(
-        <div  className='relative max-w-full mt-[40px] lg:mt-[58px]'>
-            <div className='brightness-[.2]'>
-                <Image src={bghero} alt="background image"/>
+        <div  className='relative max-w-full h-auto mt-[40px] lg:mt-[58px]'>
+            <div className='bg-[#0f172b]'>
+                <Image src={bghero} alt="background image" className='mix-blend-darken'/>
             </div>
             <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center gap-[60px] pt-[50px] lg:gap-[100px] lg:pt-[90px]  px-1 absolute inset-1" >
-                    <div className='mt-8 lg:mt-[100px] text-center lg:text-left'>
+                    <div className='mt-8 lg:mt-[100px] text-center lg:text-left animate-fade-in-left'>
                         <h1 className={`${nunito.className} text-[#fff] text-4xl lg:text-7xl font-extrabold mb-3`}>
                             Enjoy Our<br/> Delicious Meal
                         </h1>

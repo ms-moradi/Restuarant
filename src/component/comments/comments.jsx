5d3efd0d27e1fb66ab83be5ee2514bf7cb372b96
ms.moradi <ms.moradi28@gmail.com>
2023-08-21 18:44:31 +0330

    delete div container

--- src/component/comments/comments.jsx
+++ src/component/comments/comments.jsx
@@ -2,19 +2,19 @@
 import CardComment from "@/component/cardComment/cardComment";
 import Title from "../title/title";
 import { commentUser } from "@/analytics/data/data";
-import { nunito, pacifico } from "@/analytics/font/fonts";
+
 
 const Comments =()=>{
     return(
         <div className="w-full overflow-x-scroll flex flex-col flex-nowrap items-center gap-4 my-20">
             <Title title={'Testimonial'} header={'Our Clients Say!!!'}/>
-            <div className="flex justify-center gap-10">
-                {commentUser.map(comment=>(
-                    <div key={comment.id}>
-                        <CardComment name={comment.name} image={comment.image} comment={comment.comment} profession={comment.profession}/>
-                    </div>
-                ))}
-            </div>
+            <div>
+                    {commentUser.map(comment=>(
+                        <div key={comment.id}>
+                            <CardComment name={comment.name} image={comment.image} comment={comment.comment} profession={comment.profession}/>
+                        </div>
+                    ))}
+              </div>
         </div>
     )
 }

38204cc9c5c74592c9d47bab612f154d4a1bb100
ms.moradi <ms.moradi28@gmail.com>
2023-08-23 20:28:48 +0330

    add responsive style

--- src/component/banner/banner.jsx
+++ src/component/banner/banner.jsx
@@ -7,25 +7,25 @@ import hero from '@/analytics/image/hero.png';
 
 const Banner =()=>{
     return(
-        <div  className='relative max-w-full h-auto mt-[40px] lg:mt-[58px]'>
-            <div className='bg-[#0f172b]'>
-                <Image src={bghero} alt="background image" className='mix-blend-darken'/>
+        <div  className='max-w-full h-[750px] md:h-[1100px] lg:h-[800px] relative mt-[40px] lg:mt-[58px]'>
+            <div className='h-[750px] md:h-[1100px] lg:h-[750px] bg-[#0f172b]'>
+                <Image src={bghero} alt="background image" className=' h-[750px] md:h-[1100px] lg:h-[750px] mix-blend-darken object-cover'/>
             </div>
-            <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center gap-[60px] pt-[50px] lg:gap-[100px] lg:pt-[90px]  px-1 absolute inset-1" >
+            <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-center gap-[60px] pt-[50px] md:pt-[170px] lg:gap-[100px] lg:pt-[90px]  px-1 absolute inset-1" >
                     <div className='mt-8 lg:mt-[100px] text-center lg:text-left animate-fade-in-left'>
-                        <h1 className={`${nunito.className} text-[#fff] text-4xl lg:text-7xl font-extrabold mb-3`}>
+                        <h1 className={`${nunito.className} text-[#fff] text-4xl md:text-5xl lg:text-7xl font-extrabold md:mb-5 lg:mb-3`}>
                             Enjoy Our<br/> Delicious Meal
                         </h1>
-                        <div className='w-[350px] lg:w-[500px]'>
+                        <div className='w-[350px] md:w-[700px] lg:w-[500px]'>
                             <p className={`${heebo.className} text-[#fff] text-md lg:text-4 mb-6`}>
                                 Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam
                                 amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita
                                 duo justo magna dolore erat amet
                             </p>
                         </div>
-                        <Button title={'BOOk A TABLE'} width={'w-[150px] lg:w-[210px]'} height={'h-[45px] lg:h-[60px]'}/>
+                        <Button title={'BOOk A TABLE'} width={'w-[150px] md:w-[210px]'} height={'h-[45px] md:h-[60px]'}/>
                     </div>
-                    <div className='w-[300px] h-[300px] lg:w-[530px] lg:h-[530px]'>
+                    <div className='w-[300px] h-[300px] md:w-[530px] md:h-[530px]'>
                         <Image src={hero} alt='picture of the banner' layout="responsive" className='animate-spin-slow rounded-180'/>
                     </div>
                 </div>

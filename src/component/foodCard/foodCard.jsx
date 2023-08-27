c9dfb7085801cf9f981710e14175595886fce241
ms.moradi <ms.moradi28@gmail.com>
2023-08-27 12:34:07 +0330

    correct path import

--- src/component/foodCard/foodCard.jsx
+++ src/component/foodCard/foodCard.jsx
@@ -1,7 +1,7 @@
 
 import Image from "next/image";
 
-import { nunito, heebo } from "@/analytics/font/fonts";
+import { nunito, heebo } from "../../analytics/font/fonts";
 
 const FoodCard =({picture})=>{
     return(

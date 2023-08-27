3212bba788140a48d731204e9a4612c4f5509e20
ms.moradi <ms.moradi28@gmail.com>
2023-08-27 12:33:10 +0330

    change logic

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,14 +1,21 @@
 'use client'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import NavBar from '../navBar/navBar';
-import Submenu from '@/component/submenu/submenu';
+import Submenu from '../submenu/submenu';
 
 
 const Header =()=>{
+    
+    const getwidthScreen =()=>{
+        const width = screen.width
+        return (width);
+    }
 
-    const [screenUser,setScreenUser] = useState(window.screen.availWidth)
+    const [screenUser,setScreenUser] = useState(getwidthScreen())
     const [openSubMenu, setOpenSubMenu] = useState(false)
+    console.log(screenUser)
+
     return(
         <>
             <NavBar openSubMenu={openSubMenu} setOpenSubMenu={setOpenSubMenu} screenUser={screenUser} setScreenUser={setScreenUser}/>

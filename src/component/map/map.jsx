0cfe0b58b032eb59b142aca9a2608c0963e4ac35
ms.moradi <ms.moradi28@gmail.com>
2023-08-27 12:35:27 +0330

    create map component

---
+++ src/component/map/map.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+// import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
+
+
+
+const Map =()=>{
+    const position = [51.505, -0.09]
+    return(
+        <></>
+        // <MapContainer center={position} zoom={13} scrollWheelZoom={false}>           
+        //     <Marker position={position}  draggable={true} animate={true}>
+        //         <Popup>
+        //             A pretty CSS3 popup. <br /> Easily customizable.
+        //         </Popup>
+        //     </Marker>
+        // </MapContainer>
+    )
+}
+
+export default Map;

b30b550c61ae8b42c5d3b48089cf6fa11a16da12
ms.moradi <ms.moradi28@gmail.com>
2023-08-28 16:36:15 +0330

    add justify-center to div

--- src/component/miniBanner/miniBanner.jsx
+++ src/component/miniBanner/miniBanner.jsx
@@ -13,7 +13,7 @@ const MiniBanner =()=>{
             <div className="max-w-full h-96 brightness-[.2]">
                 <Image src={banner} alt="banner" className="max-w-full h-96 object-cover" />
             </div>
-            <div className="flex flex-col items-center absolute top-[130px] left-[39.6%]">
+            <div className="flex flex-col items-center justify-center absolute top-[130px] left-[39.6%]">
                 <h1 className={`${nunito.className} text-[64px] text-white font-extrabold`}>{pathname}</h1>
                 <ol className="flex gap-2 mt-2">
                     <li className={`${heebo.className} text-[#fea116] text-base`}>HOME</li>

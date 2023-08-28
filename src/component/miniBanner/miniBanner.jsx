72d62df760c166913cbf5571379b061ebc6b4df6
ms.moradi <ms.moradi28@gmail.com>
2023-08-28 16:56:53 +0330

    add flex to container and responsive style

--- src/component/miniBanner/miniBanner.jsx
+++ src/component/miniBanner/miniBanner.jsx
@@ -9,19 +9,21 @@ const MiniBanner =()=>{
     console.log(pathname)
 
     return(
-        <div className="relative mb-10">
-            <div className="max-w-full h-96 brightness-[.2]">
-                <Image src={banner} alt="banner" className="max-w-full h-96 object-cover" />
+        <div className="max-w-full h-auto flex justify-center relative mb-10">
+            <div className="max-w-full h-96  bg-[#0f172b]">
+                <Image src={banner} alt="banner" className="max-w-full h-96 mix-blend-darken  object-cover" />
             </div>
-            <div className="flex flex-col items-center justify-center absolute top-[130px] left-[39.6%]">
-                <h1 className={`${nunito.className} text-[64px] text-white font-extrabold`}>{pathname}</h1>
-                <ol className="flex gap-2 mt-2">
-                    <li className={`${heebo.className} text-[#fea116] text-base`}>HOME</li>
-                    <li className="text-white">/</li>
-                    <li className={`${heebo.className} text-[#fea116] text-base`}>PAGES</li>
-                    <li className="text-white">/</li>
-                    <li className={`${heebo.className} text-white text-base`}>{pathname}</li>
-                </ol>
+            <div className="w-auto h-auto absolute top-[130px]">
+                <div className=" flex flex-col items-center justify-center ">
+                    <h1 className={`${nunito.className} text-2xl md:text-[64px] text-white font-extrabold mb-0 md:mb-3`}>{pathname}</h1>
+                    <ol className="flex gap-2 mt-2">
+                        <li className={`${heebo.className} text-[#fea116] text-sm lg:text-base`}>HOME</li>
+                        <li className="text-white">/</li>
+                        <li className={`${heebo.className} text-[#fea116] text-sm lg:text-base`}>PAGES</li>
+                        <li className="text-white">/</li>
+                        <li className={`${heebo.className} text-white text-sm lg:text-base`}>{pathname}</li>
+                    </ol>
+                </div>
             </div>
         </div>
     )

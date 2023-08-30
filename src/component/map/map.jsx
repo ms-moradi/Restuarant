4fb1abe2329eb675a7165cfc1d5c602c25606ddd
ms.moradi <ms.moradi28@gmail.com>
2023-08-30 16:25:44 +0330

    create map but Not displayed

--- src/component/map/map.jsx
+++ src/component/map/map.jsx
@@ -1,21 +1,29 @@
 'use client'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-// import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
-
-
-
-const Map =()=>{
-    const position = [51.505, -0.09]
-    return(
-        <></>
-        // <MapContainer center={position} zoom={13} scrollWheelZoom={false}>           
-        //     <Marker position={position}  draggable={true} animate={true}>
-        //         <Popup>
-        //             A pretty CSS3 popup. <br /> Easily customizable.
-        //         </Popup>
-        //     </Marker>
-        // </MapContainer>
-    )
+const Map=()=>{
+  <>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+      crossorigin=""
+    />
+    return (
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+</>;
 }
 
 export default Map;

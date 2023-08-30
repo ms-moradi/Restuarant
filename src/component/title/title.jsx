afe8e698efbb5984e4f6372081e062d752d24891
ms.moradi <ms.moradi28@gmail.com>
2023-08-30 16:24:47 +0330

    add div container and style

--- src/component/title/title.jsx
+++ src/component/title/title.jsx
@@ -3,14 +3,14 @@ import { pacifico, nunito } from "../../analytics/font/fonts";
 
 const Title =({title, header})=>{
     return(
-        <>
+        <div className="flex flex-col items-center gap-5">
             <div className="flex justify-center items-center gap-2">
                 <div className="w-[40px] h-[2px] bg-[#fea116]"></div>
                 <h5 className={`${pacifico.className} text-xl text-[#fea116] font-normal`}>{title}</h5>
                 <div className="w-[40px] h-[2px] bg-[#fea116]"></div>
             </div>
             <h1 className={`${nunito.className} text-2xl lg:text-[40px] font-extrabold`}>{header}</h1>
-        </>
+        </div>
     )
 }
 
